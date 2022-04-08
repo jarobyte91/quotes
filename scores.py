1abebfe6fc5013c167eb90b354919858a0bcb062
Juan <jarobyte91@gmail.com>
2022-04-08 17:48:15 -0300

    organization in modules

--- scores.py
+++ scores.py
@@ -1,34 +1,7 @@
-from dash import html
-import pandas as pd
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-import dash_bootstrap_components as dbc
 import numpy as np
-
-
-def render_document(sentences, history, highlight):
-    relevant = history.query("relevance == True").sentence.tolist()
-    not_relevant = history.query("relevance != True").sentence.tolist()
-    body = []
-    for i, s in enumerate(sentences):
-        if i == highlight:
-            s = html.Strong(s)
-        if i in relevant:
-            sentence = html.Td(i, style = {"background":"lightgreen"})
-            text = html.Td(s, style = {"background":"lightgreen"})
-        elif i in not_relevant:
-            sentence = html.Td(i, style = {"background":"lightpink"})
-            text = html.Td(s, style = {"background":"lightpink"})
-        else:
-            sentence = html.Td(i)
-            text = html.Td(s)
-        row = html.Tr([sentence, text])
-        if i == highlight:
-            row.style = {"outline":"solid", "outline-width":"5px"}
-        body.append(row)
-    return dbc.Table(body, bordered = True)
-
 
 def compute_scores(
     sentences, 
